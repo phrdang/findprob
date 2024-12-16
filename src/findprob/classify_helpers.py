a1398d3143ce33c530bc19ceca2ca5d31e0e94be
Rebecca Phuonghanh Dang <rdang@berkeley.edu>
2024-12-15 17:04:12 -0800

    Implement feedback loop classification

--- src/findprob/classify_helpers.py
+++ src/findprob/classify_helpers.py
@@ -57,6 +57,34 @@ Only output valid json, and do not include any other information. Respond in the
 """
 
 
+PROMPT_TEMPLATE_FEEDBACK = """You are an experienced professor in {field} who is an expert at teaching.
+You will be given a problem from a {field} course, and you should be able to identify the topic the problem
+is trying to assess. Here are some sections of the textbook that might be related:
+{context}
+
+Here is a list of the topics that are included in this course, along with a short
+description in parentheses:
+{topics}
+
+Your task is to identify which of the above topics the problem below is trying to assess:
+{input}
+
+Before you give a final answer, you may think about which concepts might be required
+to solve the problem. Afterwards, make a list of the topics that you believe this problem covers,
+following these rules:
+
+1. You can use any topics, including those outside of the ones given in the list above. If you 
+use topic names given in the list above, they should match exactly except you must not include the 
+information in parentheses.
+2. The order of topic(s) chosen does not matter.
+
+Only output valid json, and do not include any other information. Respond in the format below:
+{{
+    "topics": ["topic choice(s) here"]
+}}
+"""
+
+
 def get_vectorstore_retriever(vec_dir, k):
     embeddings = OpenAIEmbeddings()
     vectorstore = FAISS.load_local(
@@ -85,7 +113,7 @@ def run_classifier(retrieval_chain, prompt_vars, in_dir):
             problem_path = os.path.join(dir_path, fname) 
 
             with open(problem_path) as f:
-                problem_text = f.read() # TODO fix_curly_brace?
+                problem_text = fix_curly_brace(f.read())
 
             curr_vars = prompt_vars | { "input": problem_text } # merge dictionaries
             response = retrieval_chain.invoke(curr_vars)
@@ -101,6 +129,12 @@ def run_classifier(retrieval_chain, prompt_vars, in_dir):
             classifications[problem_path] = predicted_topics
     
     return classifications
+
+
+def fix_curly_brace(s):
+    # If the problem text includes curly braces, it is interpreted as a prompt template variable
+    # so we want to replace them with {{ and }}
+    return s.replace('{', '{{').replace('}', '}}')
 
 
 def topics_given_classify(in_dir, field, retriever, topics_file):
@@ -131,8 +165,21 @@ def no_topics_classify(in_dir, field, retriever):
     return run_classifier(retrieval_chain, prompt_vars, in_dir)
 
 
-def feedback_classify():
-    pass
+def feedback_classify(in_dir, field, retriever, topics_file):
+    prompt_template = PromptTemplate(
+        template=PROMPT_TEMPLATE_FEEDBACK,
+        input_variables=["field", "context", "topics", "input"],
+    )
+    retrieval_chain = get_retrieval_chain(retriever, prompt_template)
+
+    with open(topics_file) as f:
+        topics = f.read()
+
+    prompt_vars = {
+        "field": field, 
+        "topics": topics,
+    }
+    return run_classifier(retrieval_chain, prompt_vars, in_dir)
 
 
 def save_classifications(classifications, out_file):
